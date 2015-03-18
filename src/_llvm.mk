726ed34027514cf19ca2f1a03f9bb3eee37f07b2
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-03-18 16:49:14 -0400

    _llvm.mk: helper for other makefiles

---
+++ src/_llvm.mk
@@ -0,0 +1,51 @@
+src := $(realpath $(dir $(firstword $(MAKEFILE_LIST))))
+
+####################################################################################################
+
+clang-root := $(src)/clang
+lld-root   := $(src)/lld
+llvm-root  := $(src)/llvm
+
+####################################################################################################
+
+llvm-build := $(src)/_build/llvm
+
+$(llvm-build):
+	mkdir -p $@
+
+####################################################################################################
+
+$(llvm-build)/Makefile: | $(llvm-build)
+	cd $(@D) && \
+		cmake \
+			-D CMAKE_BUILD_TYPE:STRING=Release \
+			-D CLANG_INCLUDE_DOCS:BOOL=OFF \
+			-D CLANG_INCLUDE_TESTS:BOOL=OFF \
+			-D LLVM_ENABLE_PIC:BOOL=OFF \
+			-D LLVM_EXTERNAL_CLANG_SOURCE_DIR:PATH=$(clang-root) \
+			-D LLVM_EXTERNAL_LLD_SOURCE_DIR:PATH=$(lld-root) \
+			-D LLVM_INCLUDE_EXAMPLES:BOOL=OFF \
+			-D LLVM_INCLUDE_TESTS:BOOL=OFF \
+			 $(llvm-root)
+
+####################################################################################################
+
+clang := $(llvm-build)/bin/clang
+
+$(clang): $(llvm-build)/Makefile
+	cd $(llvm-build) && $(MAKE) clang
+
+.PHONY: clang
+clang: $(clang)
+
+####################################################################################################
+
+lld := $(llvm-build)/bin/lld
+
+$(lld): $(llvm-build)/Makefile
+	cd $(llvm-build) && $(MAKE) lld
+
+.PHONY: lld
+lld: $(lld)
+
+####################################################################################################
