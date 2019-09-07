57833152f3fc5c5311b5d7a2820a95c288b95c3f
Blake Sheridan <blake.m.sheridan@gmail.com>
2019-09-07 12:43:33 -0400

    cp

--- etc/emacs.d/init.el
+++ etc/emacs.d/init.el
@@ -1,22 +1,21 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
-
-(add-to-list
-  'custom-theme-load-path
-  "~/.emacs.d/color-theme-solarized")
-
-(load-theme 'solarized t)
-
-(add-hook 'after-make-frame-functions
-  (lambda (frame)
-    (set-frame-parameter
-      frame
-      'background-mode
-      'dark)
-    (enable-theme 'solarized)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -49,37 +48,17 @@
  '(lisp-indent-offset 2)
  '(make-backup-files nil)
  '(menu-bar-mode nil)
+ '(package-selected-packages (quote (solarized-theme)))
  '(safe-local-variable-values (quote ((tab-always-indent))))
  '(show-trailing-whitespace t)
  '(standard-indent 2)
  '(tab-width 4))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-builtin-face ((t (:foreground "magenta"))))
- '(font-lock-comment-delimiter-face ((t (:foreground "black" :slant italic))))
- '(font-lock-comment-face ((t (:foreground "brightyellow" :slant italic))))
- '(font-lock-doc-face ((t (:foreground "brightgreen" :slant italic))))
- '(font-lock-function-name-face ((t (:foreground "brightred"))))
- '(font-lock-string-face ((t (:foreground "cyan"))))
- '(markdown-header-face ((t (:inherit outline-1 :underline t))))
- '(markdown-header-face-1 ((t (:inherit (outline-1 markdown-header-face)))))
- '(markdown-header-face-2 ((t (:inherit (outline-2 markdown-header-face)))))
- '(markdown-header-face-3 ((t (:inherit (outline-3 markdown-header-face)))))
- '(markdown-header-face-4 ((t (:inherit (outline-4 markdown-header-face)))))
- '(markdown-header-face-5 ((t (:inherit (outline-5 markdown-header-face)))))
- '(markdown-header-face-6 ((t (:inherit (outline-6 markdown-header-face)))))
- '(markdown-inline-code-face ((t (:inherit font-lock-constant-face :foreground "brightred"))))
- '(markdown-language-keyword-face ((t (:inherit font-lock-keyword-face))))
- '(markdown-list-face ((t (:inherit shadow))))
- '(markdown-pre-face ((t (:inherit nil :background "color-239"))))
- '(outline-1 ((t (:foreground "red"))))
- '(outline-2 ((t (:foreground "green"))))
- '(outline-3 ((t (:foreground "yellow"))))
- '(outline-4 ((t (:foreground "blue"))))
- '(outline-5 ((t (:foreground "magenta"))))
- '(outline-6 ((t (:foreground "cyan")))))
-(put 'upcase-region 'disabled nil)
+ )
+
+;(load-theme 'solarized-dark t)
+(load-theme 'solarized-light t)
